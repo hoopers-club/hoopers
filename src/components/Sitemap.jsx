06a2d57e89a9372d905c76e93a1d278b429d8aff
Rui Fernandes <r.guirrugo@gmail.com>
2021-09-19 10:00:19 +0200

    updates

--- src/components/Sitemap.jsx
+++ src/components/Sitemap.jsx
@@ -10,10 +10,10 @@ const Sitemap = ({ openSitemap }) => {
 		<div class='menus'>
 			<div class='nav'>
 				<div class='menu' onClick={openSitemap}>
-					<img src='./assets/Homepage/close.svg' alt='menu' />
+					<img src='/assets/Homepage/close.svg' alt='menu' />
 				</div>
 				<div class='logo-menu'>
-					<img src='./assets/Homepage/Logo Hoopers.svg' alt='' />
+					<img src='/assets/Homepage/Logo Hoopers.svg' alt='' />
 				</div>
 			</div>
 			<div class='menus-list'>
