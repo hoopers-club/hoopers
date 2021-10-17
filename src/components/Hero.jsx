54edaeedaf20f41b8346c50e85934d5f874db62e
Rui Fernandes <r.guirrugo@gmail.com>
2021-10-17 23:43:00 +0200

    hero and articles

---
+++ src/components/Hero.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+
+const Hero = ({ articles }) => {
+	const url = '/assets/Homepage/hero main.svg';
+	const [courtImage, setcourtImage] = useState(articles[0]);
+
+	const skip = (direction) => {
+		let currentIndex = articles.findIndex((court) => court === courtImage);
+		console.log(courtImage);
+		if (direction === 'forward') {
+			setcourtImage(articles[(currentIndex + 1) % articles.length]);
+			console.log(courtImage);
+		}
+		if (direction === 'back') {
+			if ((currentIndex - 1) % articles.length === -1) {
+				setcourtImage(articles[articles.length - 1]);
+
+				return;
+			}
+			setcourtImage(articles[(currentIndex - 1) % articles.length]);
+		}
+	};
+	return (
+		<div className='container'>
+			<div
+				className='top'
+				style={{
+					backgroundAttachment: 'fixed',
+					backgroundPosition: 'center',
+					backgroundSize: 'contain',
+					background: `linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url("${courtImage?.image}") `,
+				}}>
+				<img
+					src='/assets/courts/left.svg'
+					alt=''
+					className='changeleft'
+					onClick={() => skip('back')}
+				/>
+				<img
+					src='/assets/courts/right.svg'
+					alt=''
+					className='changeright'
+					onClick={() => skip('forward')}
+				/>
+				<div className='headline quotes'>{courtImage?.title}</div>
+				<div className='sub-headline'>{courtImage?.subname}</div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
