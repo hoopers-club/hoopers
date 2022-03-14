8f78de4123fc0c5c791c761f3889d49d743640ff
hoopersclub <info@hoopers.club>
2022-03-14 10:28:59 +0000

    Revert "run build"
    This reverts commit 46fe3de57c097825dde03e2078a2432f36e8d8a3.

--- dist/assets/script.js
+++
@@ -1,7 +0,0 @@
-const heroAnimation = bodymovin.loadAnimation({
-    container: document.getElementById('hero-animation'),
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    path: '../assets/hoopers_nft_project.json'
-});
