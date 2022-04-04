0d3491d9da6c10cbfb5bcfd65357c8ea29260846
hoopersclub <info@hoopers.club>
2022-04-04 16:29:18 +0100

    try new thing at main

--- public/main.js
+++ public/main.js
@@ -49,7 +49,7 @@ const getNbaportugal = () => {
 			let data = JSON.stringify(articles);
 
 
-			fs.writeFileSync('nba.json', data);
+			fs.writeFileSync('nba.json', JSON.stringify(articles));
 			
 			//console.log("Nba Portugal's data: " + data);
 		})
