16361b80f25828d2311418d459ab5309d33e050f
hoopersclub <info@hoopers.club>
2022-06-23 16:54:32 +0100

    changes to modal gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Gallery = ({ articles }) => {
-	const url = '/assets/Homepage/hero main.svg';
+	const url = "/assets/Homepage/hero main.svg";
 	const [courtImage, setcourtImage] = useState(articles[0]);
 
 	const skip = (direction) => {
 		let currentIndex = articles.findIndex((court) => court === courtImage);
 		console.log(courtImage);
-		if (direction === 'forward') {
+		if (direction === "forward") {
 			setcourtImage(articles[(currentIndex + 1) % articles.length]);
 			console.log(courtImage);
 		}
-		if (direction === 'back') {
+		if (direction === "back") {
 			if ((currentIndex - 1) % articles.length === -1) {
 				setcourtImage(articles[articles.length - 1]);
 
@@ -21,26 +21,32 @@ const Gallery = ({ articles }) => {
 		}
 	};
 	return (
-		<div className='gallery'>
-			<div className='left' style={{ position: 'relative', width: '100%' }}>
+		<div
+			className="gallery"
+			style={{
+				flexDirection: "column",
+				height: "100%",
+				width: "100%"
+			}}
+		>
+			<div
+				className="left"
+				style={{ position: "relative", width: "100%", height: "90%" }}
+			>
 				<img
-					src='/assets/courts/left.svg'
-					alt=''
-					className='changeleft'
-					onClick={() => skip('back')}
+					src="/assets/courts/left.svg"
+					alt=""
+					className="changeleft"
+					onClick={() => skip("back")}
 				/>
 				<img
-					src='/assets/courts/right.svg'
-					alt=''
-					className='changeright'
-					onClick={() => skip('forward')}
+					src="/assets/courts/right.svg"
+					alt=""
+					className="changeright"
+					onClick={() => skip("forward")}
 				/>
 				{courtImage && (
-					<img
-						src={courtImage}
-						alt=''
-						style={{ height: '30em', objectFit: 'cover' }}
-					/>
+					<img src={courtImage} className="gallery-item-img" alt="" />
 				)}
 				{/* {court.galleryImages.map((image, i) => (
 					<div>
--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,5 +1,5 @@
-import Modal from './Modal';
-import { useState, useEffect } from 'react';
+import Modal from "./Modal";
+import { useState, useEffect } from "react";
 
 const GalleryItem = ({ gallery }) => {
 	const [openModal, setOpenModal] = useState(false);
@@ -16,10 +16,10 @@ const GalleryItem = ({ gallery }) => {
 	}, [openModal]);
 
 	return (
-		<div className='articles-item blacked'>
+		<div className="articles-item blacked">
 			<div
 				onClick={() => handleModal()}
-				className='articles-item gallery-width '
+				className="articles-item gallery-width "
 				// 		style={{
 				//     // background:linear-gradient( rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3) ), url(https://unsplash.it/400);
 				//     backgroundRepeat: 'no-repeat',
@@ -28,24 +28,28 @@ const GalleryItem = ({ gallery }) => {
 				//     background-position:'center'}
 				// }
 				style={{
-					backgroundSize: 'cover',
-					backgroundColor: 'hsla(0%, 0%, 0%, 0.5)',
-					backgroundPosition: 'center',
-					backgroundRepeat: 'no-repeat',
+					backgroundColor: "hsla(0%, 0%, 0%, 0.5)",
 					background: `linear-gradient( rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3) ), url(${gallery?.cover})`,
-				}}>
-				<div className='esportgalleryfont'>{gallery?.title}</div>
-				<div className='article-underline'></div>
+					backgroundRepeat: "no-repeat",
+					backgroundSize: "cover",
+					backgroundPosition: "center"
+				}}
+			>
+				<div className="esportgalleryfont">{gallery?.title}</div>
+				<div className="article-underline"></div>
 				<img
-					className='esportlogomedium'
-					src='/assets/esports/Asset 1.svg'
-					alt=''
+					className="esportlogomedium"
+					src="/assets/esports/Asset 1.svg"
+					alt=""
 				/>
 			</div>
 			{openModal ? (
-				<Modal gallery={gallery?.galleryImages} handleModal={handleModal} />
+				<Modal
+					gallery={gallery?.galleryImages}
+					handleModal={handleModal}
+				/>
 			) : (
-				''
+				""
 			)}
 		</div>
 	);
